654aec9f2483c8e6bb7537568e58d0db2990cbf3
Damup1988 <damup1988@gmail.com>
2021-04-24 16:09:47 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSCode_Created_sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
