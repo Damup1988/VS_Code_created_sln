603dfb9061748901d37ddd5de3c2ac9f59daae52
Damup1988 <damup1988@gmail.com>
2021-04-24 16:43:05 +0300

    Class1 has been added

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VSCode_Created_sln
+{
+    public class Class1
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Class1(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
